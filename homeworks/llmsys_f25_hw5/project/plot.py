4abd128a504b9190908ce751964a7afbae1c6b10
Suresh Taddewadikar <tsuresh1983@gmail.com>
2025-10-31 17:58:42 +0000

    HW5, problem 1

--- homeworks/llmsys_f25_hw5/project/plot.py
+++ homeworks/llmsys_f25_hw5/project/plot.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 plt.switch_backend('Agg')
 import numpy as np
 
-def plot(means, stds, labels, fig_name):
+def plot(means, stds, labels, fig_name, title):
     fig, ax = plt.subplots()
     ax.bar(np.arange(len(means)), means, yerr=stds,
            align='center', alpha=0.5, ecolor='red', capsize=10, width=0.6)
-    ax.set_ylabel('GPT2 Execution Time (Second)')
+    ax.set_ylabel(title)
     ax.set_xticks(np.arange(len(means)))
     ax.set_xticklabels(labels)
     ax.yaxis.grid(True)
@@ -16,17 +16,25 @@ def plot(means, stds, labels, fig_name):
 
 # Fill the data points here
 if __name__ == '__main__':
-    single_mean, single_std = None, None
-    device0_mean, device0_std =  None, None
-    device1_mean, device1_std =  None, None
-    plot([mp0_mean, mp1_mean, rn_mean],
-        [mp0_std, mp1_std, rn_std],
+    single_mean, single_std = 14.98754370212555, 0.14124251494915885
+    device0_mean, device0_std = 7.718271422386169, 0.23906278408017673
+    device1_mean, device1_std = 8.201568818092346, 0.2932035662677353
+    plot([device0_mean, device1_mean, single_mean],
+        [device0_std, device1_std, single_std],
         ['Data Parallel - GPU0', 'Data Parallel - GPU1', 'Single GPU'],
-        'ddp_vs_rn.png')
+        'ddp_vs_rn.png', title="GPT2 Execution Time (Second) [H100]")
+    
+    single_tps_mean, single_tps_std = 254025.62648902385, 938.4933482871502
+    device0_tps_mean, device0_tps_std = 250288.07945994864, 1813.888516128469
+    device1_tps_mean, device1_tps_std = 250915.35066842864, 1961.6435305511584
+    plot([(device0_tps_mean + device1_tps_mean) , single_tps_mean],
+        [(device0_tps_std**2 + device1_tps_std**2)**0.5, single_tps_std],
+        ['Data Parallel - 2GPUs', 'Single GPU'],
+        'ddp_vs_rn_tps.png', title="GPT2 Tokens per Second [H100]")
 
-    pp_mean, pp_std = None, None
-    mp_mean, mp_std = None, None
-    plot([pp_mean, mp_mean],
-        [pp_std, mp_std],
-        ['Pipeline Parallel', 'Model Parallel'],
-        'pp_vs_mp.png')
+    # pp_mean, pp_std = None, None
+    # mp_mean, mp_std = None, None
+    # plot([pp_mean, mp_mean],
+    #     [pp_std, mp_std],
+    #     ['Pipeline Parallel', 'Model Parallel'],
+    #     'pp_vs_mp.png')
